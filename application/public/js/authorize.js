f5c0288f2283563b026bdc54a52be11f67db8cfd
PFGrande <2002.pedrogrande@gmail.com>
2023-04-05 03:10:37 -0700

    username checked for letter at first character

--- application/public/js/authorize.js
+++ application/public/js/authorize.js
@@ -4,10 +4,16 @@
 let usernameField = document.getElementById('reg-username');
 
 usernameField.addEventListener('change', function (usernameInput) {
-    console.log(usernameInput.currentTarget.value);
-    validateUsername(usernameInput);
+    //console.log(usernameInput.currentTarget.value);
+    validateUsername(usernameInput.currentTarget.value);
+
 });
 
+//might make it return boolean or display a list of invalid stuff
 function validateUsername(username) {
-    console.log(username.currentTarget.value);
+    console.log(username);
+    console.log(username.charCodeAt(0))
+    if (username.toUpperCase().charCodeAt(0) < 65 || username.toUpperCase().charCodeAt(0) > 90) {
+        console.log("invalid username - must start with letters A-Z or a-z");
+    }
 }
