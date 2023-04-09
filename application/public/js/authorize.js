27e3a1c51784b4fbc6c13a78b9d8e2415cadb447
PFGrande <2002.pedrogrande@gmail.com>
2023-04-08 23:01:53 -0700

    added username check for first char and alphanumeric chars

--- application/public/js/authorize.js
+++ application/public/js/authorize.js
@@ -11,9 +11,37 @@ usernameField.addEventListener('change', function (usernameInput) {
 
 //might make it return boolean or display a list of invalid stuff
 function validateUsername(username) {
-    console.log(username);
+    let missingCriteria = "Username requirements:";
+    //console.log(username);
     console.log(username.charCodeAt(0))
     if (username.toUpperCase().charCodeAt(0) < 65 || username.toUpperCase().charCodeAt(0) > 90) {
-        console.log("invalid username - must start with letters A-Z or a-z");
+        //console.log("invalid username - must start with letters A-Z or a-z");
+        missingCriteria += "\nstart with characters: A-Z or a-z";
     }
+
+    let alphanumericCount = checkAlphanumeric(username);
+
+    if (alphanumericCount < 3) {
+        missingCriteria += "\nmust contain 3 or more alphanumeric characters"
+    }
+
+    if (missingCriteria !== "Username requirements:") {
+        alert(missingCriteria)
+    }
+}
+
+//Increments count if it is between numerical and alphabetic characters
+function checkAlphanumeric (name) {
+    let count = 0;
+
+    for (let i = 0; i < name.length; i++) {
+        if (name.charCodeAt(i) >= 48 && name.charCodeAt(i) <= 57) {
+            count++;
+        } else if (name.toUpperCase().charCodeAt(i) >= 65 && name.toUpperCase().charCodeAt(i) <= 90) {
+            count++;
+        }
+    }
+    return count;
+
+
 }
