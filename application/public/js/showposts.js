4a43f54f39f0c3bf11f457d1bd55ebbf01990495
PFGrande <2002.pedrogrande@gmail.com>
2023-04-12 16:55:45 -0700

    images appear in index page, working on card layout

--- application/public/js/showposts.js
+++ application/public/js/showposts.js
@@ -1,11 +1,14 @@
 const postsDataURL = "https://jsonplaceholder.typicode.com/albums/2/photos";
+console.log(fetchWithJSON())
 async function fetchWithJSON() {//async function creates promise
     try {
         let response = await fetch(postsDataURL); //fetch returns promise, wait for promise resolution before continuing in code
         let data = await response.json(); //converts data to JavaScript object
         let elements = data.map(buildCard);
+        document.getElementById("posts-grid").append(...elements);
+        //console.log(elements.length); length of array = number of cards
     } catch (err) { //promise rejected
-        console.log(err); //prints error in console
+        console.log("error: " + err); //prints error in console
     }
 }
 
@@ -15,10 +18,10 @@ function buildCard(data) {
 
     let cardImg = document.createElement("img");
     cardImg.setAttribute("class","card-img");
-    cardImg.setAttribute("src","data.thumbnailUrl");
+    cardImg.setAttribute("src",data.thumbnailUrl);
 
-    let cardInfo = document.createElement('div');
-    cardInfo.setAttribute('class','card-desc');
+    let cardDesc = document.createElement('div');
+    cardDesc.setAttribute('class','card-desc');
 
     let cardTitle = document.createElement("h1");
     cardTitle.setAttribute("class", "card-title");
@@ -28,10 +31,10 @@ function buildCard(data) {
     cardUrl.setAttribute("href", "card-url");
     cardUrl.appendChild(document.createTextNode(data.url));
 
-    cardDiv.appendChild(cardInfo);
-    cardInfo.appendChild(cardTitle);
-    cardInfo.appendChild(cardImg);
-    cardInfo.appendChild(cardUrl);
+    cardDiv.appendChild(cardDesc);
+    cardDesc.appendChild(cardTitle);
+    cardDesc.appendChild(cardImg);
+    cardDesc.appendChild(cardUrl);
 
     //cardDiv.addEventListener('click', fadeOut);
 
