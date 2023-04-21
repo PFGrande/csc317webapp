606f3dfb04b44abe4108c088ba094ff88b65d40b
PFGrande <2002.pedrogrande@gmail.com>
2023-04-20 23:09:23 -0700

    created header and nav bar partials, also added main tags across pages

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Pedro" });
+  res.render('index', { title: 'CSC 317 App / Home', name:"Pedro", description: 'Welcome to the home of Americas best lineups!'})
 });
 
 router.get('/login', function (req, res) {
-  res.render('login');
+  res.render('login', {title: 'Log in', description: 'enter account credentials'});
 });
 
-router.get('/register', function(req, res) {
-  res.render('register');
+router.get('/registration', function(req, res) {
+  res.render('registration', {title: 'Registration', Description: 'Please register an account to post videos'});
 });
 
 router.get('/postvideo', function(req, res) {
-  res.render('postvideo');
+  res.render('postvideo', {title: 'Upload', Description: 'Here you can upload video files'});
 });
 
 router.get('/viewpost', function (req, res) { //set client ID later on.
-  res.render('viewpost');
+  res.render('viewpost', {title: 'View Posts', description: 'Video for your viewing pleasure'});
 });
 
 router.get('/profile', function (req, res) {
-  res.render('profile')
+  res.render('profile', {title: 'Profile'})
 });
 
 module.exports = router;
