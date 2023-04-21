00b7bfc1164d4783042b0407ece73a57e698787d
PFGrande <2002.pedrogrande@gmail.com>
2023-04-20 21:19:43 -0700

    added routing to hbs files, updated link and script paths in index file

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Pedro" });
 });
 
+router.get('/login', function (req, res) {
+  res.render('login');
+});
+
+router.get('/register', function(req, res) {
+  res.render('register');
+});
+
+router.get('/postvideo', function(req, res) {
+  res.render('postvideo');
+});
+
+router.get('/viewpost', function (req, res) { //set client ID later on.
+  res.render('viewpost');
+});
+
+router.get('/profile', function (req, res) {
+  res.render('profile')
+});
+
 module.exports = router;
