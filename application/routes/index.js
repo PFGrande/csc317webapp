694c3c6c0dae9f08bbf5a3a67f6f858af089a99f
PFGrande <2002.pedrogrande@gmail.com>
2023-04-20 23:26:12 -0700

    added specific javascript files per page

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App / Home', name:"Pedro", description: 'Welcome to the home of Americas best lineups!'})
+  res.render('index', { title: 'CSC 317 App / Home', name:"Pedro", description: 'Welcome to the home of Americas best lineups!', javaScript: ['showposts.js']})
 });
 
 router.get('/login', function (req, res) {
@@ -11,11 +11,11 @@ router.get('/login', function (req, res) {
 });
 
 router.get('/registration', function(req, res) {
-  res.render('registration', {title: 'Registration', Description: 'Please register an account to post videos'});
+  res.render('registration', {title: 'Registration', description: 'Please register an account to post videos', javaScript: ['authorize.js']});
 });
 
 router.get('/postvideo', function(req, res) {
-  res.render('postvideo', {title: 'Upload', Description: 'Here you can upload video files'});
+  res.render('postvideo', {title: 'Upload', description: 'Here you can upload video files'});
 });
 
 router.get('/viewpost', function (req, res) { //set client ID later on.
