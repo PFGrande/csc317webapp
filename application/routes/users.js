7211274e390ba8cb43f6daf95f722276cd8de3ea
PFGrande <2002.pedrogrande@gmail.com>
2023-05-07 00:46:10 -0700

    added notes to users.js and added confirmPassword input name

--- application/routes/users.js
+++ application/routes/users.js
@@ -3,6 +3,16 @@ var router = express.Router();
 var db = require('../conf/database');
 /* GET users listing. (localhost:3000/users) */
 /*keep watching video at 1:06:55 : CSC 317 Joins, db setup...*/
+
+/*
+Notes:
+  The middleware is loaded into the stack after a request is fulfilled (it is also loaded into the stack before
+  code is executed).
+    This means that every request will have the same middleware.
+  The order of middleware functions is that of which they are defined.
+    .use indicates a middleware function being added to the stack.
+ */
+
 /*prints table elements as objects to the console (for debugging purposes)*/
 router.get('/', async function(req, res, next) {
   //res.send('respond with a resource');
@@ -25,9 +35,12 @@ router.get('/', async function(req, res, next) {
 });
 
 /*routing for localhost:3000/user/registration */
+//Left off at 36:13, remember to rewatch bcrypt lecture
+//request handler for the registration page, parameters: route path, handler function
 router.post('/registration', async function(req, res, next) {
   console.log(req.body);
   res.end();
+  //uniqueness checks will happen here
 });
 
 
