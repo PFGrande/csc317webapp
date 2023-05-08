d6677e21d6fe4ee41279159d6332c59fd226d660
PFGrande <2002.pedrogrande@gmail.com>
2023-05-08 00:25:02 -0700

    user can be created (without password encryption)

--- application/routes/users.js
+++ application/routes/users.js
@@ -48,7 +48,7 @@ router.post('/registration', async function(req, res, next) {
   //destructure json object.
   let {username, email, password} = req.body;
 
-  //Check uniqueness of values:
+  //Check uniqueness of values: A sessionID can be created after the user's account is inserted into the database (not necessary)
   try { //promise
     //Username uniqueness check
     var [rows, fields] = await db.execute(`select id from users where username=?;`,[username]); //don't use ${username}, could lead to SQL injection
@@ -67,12 +67,24 @@ router.post('/registration', async function(req, res, next) {
       return res.redirect('/registration');//function breaks & website refresh if matching email found (rows.length > 0)
     }
 
+    //encrypt password before inserting it into db
+
     //insert data into the database (inserts rows into DB)
     //result object = new row being input, fields is the columns?
     var [resultObject, fields] = await db.execute(`INSERT INTO users (username, email, password) value (?,?,?);`, [username, email, password]);
 
-    console.log(resultObject); //print object being input into user table
-    res.end(); //ends request
+    //console.log(resultObject); //print object being input into user table, result object contains the id for new row :)
+    //res.end(); //ends request
+
+    //response:
+    //resultObject will not be null & the affected rows should be 1.
+    //This statement checks for a successful insert.
+    if (resultObject && resultObject.affectedRows) { //if resultObject != NULL && resultObject.affectedRows != NULL
+      res.redirect('/login'); //object inserted into row, proceed to login page
+    } else {
+      return res.redirect('/registration'); //object not inserted, reload registration page
+    }
+
 
   }catch (error) {
     next(error);
